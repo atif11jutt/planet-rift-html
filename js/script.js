3e77ed4549951ac18dcd491c375298ca1ed8fab0
atif11jutt <atif11jutt@gmail.com>
2022-06-06 00:12:53 +0500

    upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* ------ OWL Slider ------ */
+    
+    $("#home-slider").owlCarousel({
+        items: 1,
+        autoplay: 1300,
+        smartSpeed: 1300,
+        slideBy: 1,
+        loop: true,
+        dots: false,
+        nav: false,
+    });
+
+
+    
